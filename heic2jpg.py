6797ed492ba26ccdbd1f6edb894848dec7b3afaa
Karan Saini <37518297+qurbat@users.noreply.github.com>
2023-09-12 21:53:35 +0530

    Update heic2jpg.py

--- heic2jpg.py
+++ heic2jpg.py
@@ -7,6 +7,8 @@ def convert_heic_to_jpg(directory, delete_heic):
             if filename.lower().endswith(".heic"): 
                 print('Converting %s...' % os.path.join(root, filename))
                 subprocess.run(["magick", "%s" % os.path.join(root, filename), "%s" % (filename[0:-5] + '.jpg')], cwd=root)
+                if not os.path.exists(os.path.join(root, filename[0:-5] + '.jpg')):
+                    print('Error: Converted file %s does not exist. Is the ImageMagick binary in PATH?' % os.path.join(root, filename[0:-5] + '.jpg'))
                 converted_files[os.path.join(root, filename)] = True
                 continue
 
